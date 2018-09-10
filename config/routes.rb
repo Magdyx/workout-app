0e4a963a7fb6da3341f6d091c5562a40ddcd9827
Mohamed Magdy <mohamed.magdy@pharos-solution.de>
2018-09-10 17:22:01 +0200

    add routes, index and new to exercise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'exercises/index'
+
   devise_for :users
   get 'dashboards/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "dashboards#index"
+
+  resources :users do
+    resources :exercises
+  end
 end
