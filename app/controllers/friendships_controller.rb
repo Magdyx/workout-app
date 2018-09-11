b57731ee1a99fb3269931045fe8f409bba258988
Mohamed Magdy <mohamed.magdy@pharos-solution.de>
2018-09-11 17:24:17 +0200

    adding remove friend action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -9,6 +9,23 @@ class FriendshipsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @friend = Friendship.find(params[:id]).friend
+    @exercises = @friend.exercises
+  end
+
+  def destroy
+    @friendship = Friendship.find(params[:id])
+    friendname = @friendship.friend.full_name
+
+    if @friendship.destroy
+      flash[:notice] = "#{friendname} unfollowed."
+    else
+      flash.now[:alert] = "#{friendname} could not be unfollowed."
+    end
+    redirect_to user_exercises_path(current_user)
+  end
+
   private
 
   def friendship_params
