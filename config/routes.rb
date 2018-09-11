2e9b59cf1fd50f1c7360d7f273b01bf9b6b2af02
Mohamed Magdy <mohamed.magdy@pharos-solution.de>
2018-09-11 14:07:14 +0200

    add first and last name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'exercises/index'
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
+  get 'exercises/index'
   get 'dashboards/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
